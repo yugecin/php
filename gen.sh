839ad54e89b3584251c91c9fcf4d86ca888f38b9
yugecin <yugecin@users.noreply.github.com>
2018-09-01 13:27:29 +0200

    early blog processing

--- gen.sh
+++ gen.sh
@@ -1,9 +1,50 @@
 #!/usr/bin/env bash
 
+# $1 outputfile
+# $2 inputstring (as in pages/_template.txt format)
+# $3 breadcrumbs or anything that goes between nav and page content or nothing
+function makepage {
+	echo "$SKELETON1" > $1
+	echo "$2" | head -n1 >> $1
+	echo "$SKELETON23" >> $1
+	echo "<header><a href=\"$1~INTERNALLINKINV~\">~LIGHTSOFF~</a></header>" >> $1
+	echo "$NAV" >> $1
+	if [ ! -z "$3" ]
+	then
+		echo "$3" >> $1
+	fi
+	echo "$2" | sed -f tohtml.sed >> $1
+	echo "$SKELETON4" >> $1
+	echo "$2" | sed -n '3p' >> $1
+	echo "$SKELETON5" >> $1
+	PAGEFILES+=($1)
+}
+
+PAGEFILES=()
+
+# remove existing generated pages
 ls -1 *.html | grep -v "^_" | xargs rm
 
+# collect blog posts
+mapfile -t _BLOGPOSTS < <(find blog/*.txt -maxdepth 1 -type f | grep -v "blog/_")
+BLOGPOSTS=
+for IX in "${_BLOGPOSTS[@]}"
+do
+	TS=$(head -n1 $IX)
+	if [[ "$TS" = "WIP" ]]
+	then
+		continue
+	fi
+	BLOGPOSTS=$BLOGPOSTS$'\n'"$TS $IX"
+done
+
+BLOGPOSTS=$(echo "$BLOGPOSTS" | tail -n +2 | sort -n)
+readarray -t BLOGPOSTS <<<"$BLOGPOSTS"
+
+# collect pages
 mapfile -t PAGES < <(find pages/*.txt -maxdepth 1 -type f | grep -v "pages/_")
 
+# make nav
 NAV=
 for IX in "${PAGES[@]}"
 do
@@ -14,6 +55,7 @@ done
 
 NAV="<nav>${NAV#*|}</nav><hr/>"
 
+# make skeleton
 SKELETON1=$(sed -f minhtml.sed _skeleton1.html)
 SKELETON23=$(sed -f minhtml.sed _skeleton2.html)
 SKELETON23=$SKELETON23$(sed -f mincss.sed style.css)
@@ -21,27 +63,30 @@ SKELETON23=$SKELETON23$(sed -f minhtml.sed _skeleton3.html)
 SKELETON4=$(sed -f minhtml.sed _skeleton4.html)
 SKELETON5=$(sed -f minhtml.sed _skeleton5.html)
 
-PAGEFILES=()
+# make blog posts
+for IX in "${BLOGPOSTS[@]}"
+do
+	INPUTFILE=${IX#* }
+	PF=${INPUTFILE##*/}
+	PF=${PF/_/-}
+	PF=blog-${PF/.txt/.html}
+	echo "$PF"
+	#BLOGPOSTLIST+=(${IX%% *} $PF)
+	makepage "$PF" "$(tail -n +3 $INPUTFILE)"
+done
 
+echo ""
+
+# make pages
 for IX in "${PAGES[@]}"
 do
 	PF=${IX##*/*_}
 	PF=${PF%.txt*}.html
-	PAGEFILES+=($PF)
-
 	echo $PF
-
-	echo $SKELETON1 > $PF
-	head -n1 $IX >> $PF
-	echo $SKELETON23 >> $PF
-	echo "<header><a href=\"$PF~INTERNALLINKINV~\">~LIGHTSOFF~</a></header>" >> $PF
-	echo $NAV >> $PF
-	sed -f tohtml.sed <$IX >> $PF
-	echo $SKELETON4 >> $PF
-	sed -n '3p' <$IX >> $PF
-	echo $SKELETON5 >> $PF
+	makepage "$PF" "$(cat $IX)"
 done
 
+# light and dark theme for every page
 for FROM in "${PAGEFILES[@]}"
 do
 	TO=${FROM%.html*}_d.html
